fb1493716efda01b6ba4e71feaed08fac9b1a5b4
Mateus Queiroz Logrado <mateusqueirozlogrado@gmail.com>
2025-02-15 19:04:30 -0300

    boioioiog

--- js/click.js
+++ js/click.js
@@ -1,6 +1,7 @@
 let click = document.querySelector("#click")
 let coin = 0
 
+
 click.addEventListener("click", ()=>{
     let displayCoin = document.getElementById("displayCoin")
     let clickCoin = 1
@@ -10,3 +11,17 @@ click.addEventListener("click", ()=>{
 
 
 })
+
+click.addEventListener('click', ()=>{
+    let click2 = document.getElementById("click")
+    click2.style.width = "270px"
+    click2.style.height = "270px"
+    setTimeout(normal, 500)
+})
+
+function normal(){
+    let click2 = document.getElementById("click")
+
+    click2.style.width = "280px"
+    click2.style.height = "280px"
+}
